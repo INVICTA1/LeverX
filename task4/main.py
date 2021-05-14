d66f9925cb556a680297f8ec2285c12fdc044c69
Maksim <makc.pinsk@gmail.com>
2021-05-14 12:00:30 +0300

    added xml and json output

--- task4/main.py
+++ task4/main.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import mysql.connector
 import json
+from lxml import etree
 from mysql.connector import Error
 from models.students import *
 from models.rooms import *
@@ -35,17 +36,16 @@ def call_procedure(cursor, procedure):
     try:
         cursor.callproc(procedure)
         for result in cursor.stored_results():
-            print(result)
-            print(result.fetchall())
             return result.fetchall()
     except (BaseException, Error) as e:
         raise ("Can't call procedure", e)
 
 
-def output_json(result: dict, name_procedure):
+def output_json(result, name_procedure: str):
     """Output data to a JSON file"""
 
-    name_file = r'result\\' + name_procedure + '.json'
+    name_file = r'result\json\\' + name_procedure + '.json'
+    print(result)
     try:
         with open(name_file, 'w+', encoding='utf-8', ) as file:
             json.dump(result, file, ensure_ascii=False, indent=4)
@@ -53,12 +53,34 @@ def output_json(result: dict, name_procedure):
         raise Exception("Can't' output JSON data", e)
 
 
+def output_xml(result, name_procedure: str):
+    """Output data to a XML file"""
+
+    name_file = r'result\xml\\' + name_procedure + '.xml'
+    try:
+        data = etree.Element('data')
+        for row in result:
+            if len(row) == 2:
+                room = etree.SubElement(data, 'room')
+                room.text = row[0]
+                num_students = etree.SubElement(room, 'num_students')
+                num_students.text = str(row[1])
+            elif len(row) == 1:
+                room = etree.SubElement(data, 'room')
+                room.text = row[0]
+        data = etree.ElementTree(data)
+        with open(name_file, 'wb') as file:
+            data.write(file, xml_declaration=True, pretty_print=True)
+    except Exception as MyException:
+        raise MyException
+
+
 def main():
     procedures = [
-        # 'usp_find_list_rooms_with_students',
+        'usp_find_list_rooms_with_students',
         'usp_top5_rooms_with_min_average_age',
-        # 'usp_top5_rooms_with_diff_age',
-        # 'usp_find_list_rooms_with_mixed_students',
+        'usp_top5_rooms_with_diff_age',
+        'usp_find_list_rooms_with_mixed_students',
     ]
     parser = get_parser_arguments()
     namespace = parser.parse_args(sys.argv[1:])
@@ -71,13 +93,13 @@ def main():
         # RoomsDB.load_rooms_to_db(config['database'], rooms, cursor)
         # StudentDB.load_students_to_db(config['database'], students, cursor)
         # добавить индекс
-        conn.commit()
+        # conn.commit()
         for procedure in procedures:
             result = call_procedure(cursor, procedure)
             if namespace.format == 'xml':
-                output_xml(result)
+                output_xml(result, procedure)
             elif namespace.format == 'json':
-                output_json(result,procedure)
+                output_json(result, procedure)
         cursor.close()
         conn.close()
 
